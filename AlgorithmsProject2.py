b513ffa9e794e3565ab1ea7a2e9b03d0fe088337
Khris Pace <77419079+KTP98@users.noreply.github.com>
2021-10-11 11:16:34 -0500

    insertion and bubble sort

--- AlgorithmsProject2.py
+++ AlgorithmsProject2.py
@@ -14,6 +14,30 @@ def partition(array, begin, end, comparisons):
     array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
     return pivot_idx, comparisons
 
+
+#insertion sort function
+#code: https://medium.com/@george.seif94/a-tour-of-the-top-5-sorting-algorithms-with-python-code-43ea9aa02889
+# modified to get number of comparisons
+def insertion_sort(arr):
+    
+    comparisons = 0    
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+        comparisons += 1 # have to count here as there is one comparison in the loop below even if the 
+                         # the evaluation is false
+        while pos > 0 and arr[pos - 1] > cursor:
+            # Swap the number down the list
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+            comparisons += 1
+        # Break and do the final swap
+        arr[pos] = cursor
+
+    return arr, comparisons
+
+#Quick sort
+# modified to get number of comparisons
 def quick_sort_recursion(array, begin, end):
     comparisons = 0
     if begin < end:
@@ -27,14 +51,42 @@ def quick_sort(array, begin=0, end=None):
         end = len(array) - 1
     
     return 0, quick_sort_recursion(array, begin, end)
+# Appently in the return statment above quicksort will return the empty array 
+# as well so in order to avoid this return had to have a 0 after it.
 
-# https://stackoverflow.com/questions/18761766/mergesort-with-python
+
+#bubble sort 
+#code: https://medium.com/@george.seif94/a-tour-of-the-top-5-sorting-algorithms-with-python-code-43ea9aa02889
+# modified to get number of comparisons
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    
+    x = -1
+    comparisons = 0
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            comparisons += 1
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+                    
+    return arr, comparisons
+
+# Merge sort
+# https://www.geeksforgeeks.org/merge-sort/
+# modified to get number of comparisons
 def mergeSort(arr, comparisons = 0):
     
     if len(arr) >1: 
         #Finding the mid of the array 
         mid = len(arr)//2 
-        # Dividing the array elements 
+        # Divide array elements 
         L = arr[:mid] 
         # into 2 halves 
         R = arr[mid:] 
@@ -88,22 +140,20 @@ def randomArray(size, type = 'd'):
     return array
 
 
-def testRunner(sortFunction, arrayType, tests = 3):
-    
-    mag = 100  # size of the array, first execution the size is 50
-    comparisons = []  #  the number of comparisons per run
-    elapsedTime = []  # the elapsed time per run
+def testTime(sortFunction, arrayType, tests = 3):
+    arrsize = 100  # size of the array
+    comparisons = []  #  number of comparisons per run
+    elapsedTime = []  # elapsed time per run
     for i in range(tests):
-        array = randomArray(mag, arrayType)
-        print('\nTesiting with ' + str(mag) + ' elements')
+        array = randomArray(arrsize, arrayType)
+        print('\nTesiting with ' + str(arrsize) + ' elements')
         t0 = time.perf_counter()
-        # ignoring the returned array with _
         _, comp = sortFunction(array)
         comparisons.append(comp)
         t1 = time.perf_counter()
         elapsedTime.append(round(t1 - t0, 5))
-        print('Sort of ' + str(mag) + ' elements had an elapsed time of ' + str(elapsedTime[i]) + ' with ' + str(comparisons[i]) + ' comparisons.')
-        mag *= 10
+        print('Sorting of ' + str(arrsize) + ' elements had an elapsed time of ' + str(elapsedTime[i]) + 's, with ' + str(comparisons[i]) + ' comparisons.')
+        arrsize *= 10
 
     return comparisons, elapsedTime
 
@@ -114,31 +164,31 @@ def writeFile(fileName, itemsList, format="%d\n"):
 def main():
     
     print("\n**** QUICK-SORT BEST RUN TIME ****")
-    comps, elapsedTime = testRunner(quick_sort, 'd', 5)
+    comps, elapsedTime = testTime(quick_sort, 'd', 5)
     
     writeFile('quicksort-avg-compares.txt', comps)
     writeFile('quicksort-avg-elapsedtimes.txt', elapsedTime, "%f\n")
 
     print('\n**** QUICK-SORT WORST RUN TIME - Sorted array ****')
-    comps, elapsedTime = testRunner(quick_sort, 's')
+    comps, elapsedTime = testTime(quick_sort, 's')
    
     writeFile('quicksort-worst-compares.txt', comps)
     writeFile('quicksort-worst-elapsedtimes.txt', elapsedTime, "%f\n")
     
     print("\n**** MERGE-SORT AVERAGE RUN TIME ****")
-    comps, elapsedTime = testRunner(mergeSort, 'd', 5)
+    comps, elapsedTime = testTime(mergeSort, 'd', 5)
     
     writeFile('mergesort-avg-compares.txt', comps)
     writeFile('mergesort-avg-elapsedtimes.txt', elapsedTime, "%f\n")
     
     print("\n**** MERGE-SORT BEST RUN TIME ****")
-    comps, elapsedTime = testRunner(mergeSort, 's', 5)
+    comps, elapsedTime = testTime(mergeSort, 's', 5)
     
     writeFile('mergesort-best-compares.txt', comps)
     writeFile('mergesort-best-elapsedtimes.txt', elapsedTime, "%f\n")
     
     print("\n**** MERGE-SORT WORST RUN TIME ****")
-    comps, elapsedTime = testRunner(mergeSort, 'm', 5)
+    comps, elapsedTime = testTime(mergeSort, 'm', 5)
     
     writeFile('mergesort-worst-compares.txt', comps)
     writeFile('mergesort-worst-elapsedtimes.txt', elapsedTime, "%f\n")
