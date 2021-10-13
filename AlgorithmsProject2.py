a5f46e1ac1615a809a1591f49cd6f6b391d7c769
Khris Pace <77419079+KTP98@users.noreply.github.com>
2021-10-13 12:33:02 -0500

    Insertion sort moved

--- AlgorithmsProject2.py
+++ AlgorithmsProject2.py
@@ -14,6 +14,24 @@ def partition(array, begin, end, comparisons):
     array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
     return pivot_idx, comparisons
 
+#Quick sort
+# modified to get number of comparisons
+def quick_sort_recursion(array, begin, end):
+    comparisons = 0
+    if begin < end:
+        pivot_idx, comparisons = partition(array, begin, end, comparisons)
+        comparisons += quick_sort_recursion(array, begin, pivot_idx-1)
+        comparisons += quick_sort_recursion(array, pivot_idx+1, end)
+    return comparisons
+
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    
+    return 0, quick_sort_recursion(array, begin, end)
+# Appently in the return statment above quicksort will return the empty array 
+# as well so in order to avoid this return had to have a 0 after it.
+
 
 #insertion sort function
 #code: https://medium.com/@george.seif94/a-tour-of-the-top-5-sorting-algorithms-with-python-code-43ea9aa02889
@@ -35,24 +53,6 @@ def insertion_sort(arr):
         arr[pos] = cursor
 
     return arr, comparisons
-
-#Quick sort
-# modified to get number of comparisons
-def quick_sort_recursion(array, begin, end):
-    comparisons = 0
-    if begin < end:
-        pivot_idx, comparisons = partition(array, begin, end, comparisons)
-        comparisons += quick_sort_recursion(array, begin, pivot_idx-1)
-        comparisons += quick_sort_recursion(array, pivot_idx+1, end)
-    return comparisons
-
-def quick_sort(array, begin=0, end=None):
-    if end is None:
-        end = len(array) - 1
-    
-    return 0, quick_sort_recursion(array, begin, end)
-# Appently in the return statment above quicksort will return the empty array 
-# as well so in order to avoid this return had to have a 0 after it.
 
 
 #bubble sort 
@@ -195,8 +195,3 @@ def main():
 
 if __name__ == "__main__":
         main()
-
-
-
-
-
